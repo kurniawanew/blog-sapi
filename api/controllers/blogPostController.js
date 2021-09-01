5425d983927f888bb4e9ce278bf56c806c6b1ea9
Kurniawan Eko Wasono <kurniawanew@gmail.com>
2021-09-02 01:42:21 +0700

    add controller for add new post and get it all

---
+++ api/controllers/blogPostController.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Article = mongoose.model('Article');
+
+exports.getAllArticle = function(req, res) {
+    Article.find({}, function(err, article) {
+        if (err)
+            res.send(err);
+        es.json(article);
+    });
+};
+
+
+exports.postANewArticle = function(req, res) {
+    var new_article = new Article(req.body);
+    new_article.save(function(err, article) {
+        if (err)
+            res.send(err);
+        res.json(article);
+    });
+};
