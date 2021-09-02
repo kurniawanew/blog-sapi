6718a2d985a8ba256db9618706e1d08466532bdc
Kurniawan Eko Wasono <kurniawanew@gmail.com>
2021-09-03 01:12:57 +0700

    using cache on controller

--- api/controllers/blogPostController.js
+++ api/controllers/blogPostController.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Article = mongoose.model('Article');
+    Article = mongoose.model('Article'),
+    clearKey = require("../../services/cache");
 
-exports.getAllArticle = function(req, res) {
+exports.getAllArticle = async function(req, res) {
     var where = {};
     var author = req.query.author;
     var query = req.query.query;
@@ -13,7 +14,7 @@ exports.getAllArticle = function(req, res) {
     if(query !== undefined) {
         where.$or = [{title: { $regex: query, $options: 'i'}}, {body: { $regex: query, $options: 'i'}}]
     }
-    Article.find(where, {
+    await Article.find(where, {
         "_id": 0,
         "author": 1, 
         "title": 1, 
@@ -23,15 +24,16 @@ exports.getAllArticle = function(req, res) {
         if (err)
             res.send(err);
         res.json(article);
-    });
+    }).cache({time: 120});
 };
 
 
-exports.postANewArticle = function(req, res) {
+exports.postANewArticle = async function(req, res) {
     var new_article = new Article(req.body);
-    new_article.save(function(err, article) {
+    await new_article.save(function(err, article) {
         if (err)
             res.send(err);
+        clearKey(Article.collection.collectionName);
         res.json(article);
     });
 };
