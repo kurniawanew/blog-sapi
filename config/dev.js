e2f47bc75afcdba79348a892dbfbcd725a4b3f77
Kurniawan Eko Wasono <kurniawanew@gmail.com>
2021-09-03 05:33:30 +0700

    prepare for dockerize

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//     mongoURI: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`,
-//     redisPort: process.env.REDIS_PORT,
-//     redisHost: process.env.REDIS_HOST
-//   };
-
 module.exports = {
-    mongoURI: 'mongodb://localhost/Articledb',
-    redisPort: "6379",
-    redisHost: "redis"
+    mongoURI: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`,
+    redisPort: process.env.REDIS_PORT,
+    redisHost: process.env.REDIS_HOST
   };
+
+// module.exports = {
+//     mongoURI: 'mongodb://localhost:27017/Articledb',
+//     redisPort: "6379",
+//     redisHost: "localhost"
+//   };
